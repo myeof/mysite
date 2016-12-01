3e4355a67b8f0361c6928fbefc7d92bdb1be3a7f
myeof <183595015@qq.com>
2016-12-01 09:59:39 +0800

    commit

---
+++ blog/models.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+
+# Create your models here.
+
+
+class User(AbstractBaseUser):
+    pass
+
+    class Meta:
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.username
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30,verbose_name='分类名称')
+    index = models.IntegerField(verbose_name='分类排序')
+
+    class Meta:
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.username
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=16,verbose_name="标签名称")
+
+    class Meta:
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.username
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=50, verbose_name='文章标题')
+    desc = models.CharField(max_length=100, verbose_name='文字描述')
+    content = models.TextField(verbose_name='文字内容')
+    click_count = models.IntegerField(default=0, verbose_name='点击次数')
+    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+    user = models.ForeignKey(User, verbose_name='作者')
+    category = models.ForeignKey(Category, verbose_name='分类')
+    tag = models.ForeignKey(Tag, verbose_name='标签')
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+        ordering = ['-date_publish']
+
+    def __unicode__(self):
+        return self.username
+
+
+class Comment(models.Model):
+    name = models.CharField(max_length=16, verbose_name='名字')
+    email = models.EmailField(verbose_name='邮箱')
+    content = models.CharField(max_length=2000, verbose_name='评论内容')
+    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='评论时间')
+    pid = models.ForeignKey('self', blank=True, null=True, verbose_name='父级评论')
+
+    class Meta:
+        verbose_name = '评论'
+        verbose_name_plural = verbose_name
+        ordering = ['-date_publish']
+
+    def __unicode__(self):
+        return self.username
+
+
+class Ad(models.Model):
+    title = models.CharField(max_length=50, verbose_name='广告标题')
+    desc = models.CharField(max_length=50, verbose_name='广告描述')
+    img_url = models.ImageField(upload_to='ad/%Y/%m', verbose_name='图片路径')
+    callback_url = models.URLField(null=True, blank=True, verbose_name='回调url')
+    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+    index = models.IntegerField(default=999, verbose_name='排列顺序（从小到大）')
+
+    class Meta:
+        verbose_name = '广告'
+        verbose_name_plural = verbose_name
+        ordering = ['index', 'id']
+
+    def __unicode__(self):
+        return self.title
+
+
+class Links(models.Model):
+    title = models.CharField(max_length=50, verbose_name='友情连接标题')
+    desc = models.CharField(max_length=200, verbose_name='友情连接描述')
+    callback_url = models.URLField(verbose_name='友情连接url')
+    date_publish = models.DateTimeField(auto_now_add=True, verbose_name='发布时间')
+    index = models.IntegerField(default=999, verbose_name='排列顺序（从小到大）')
+
+    class Meta:
+        verbose_name = '友情连接'
+        verbose_name_plural = verbose_name
+        ordering = ['index', 'id']
+
+    def __unicode__(self):
+        return self.title
